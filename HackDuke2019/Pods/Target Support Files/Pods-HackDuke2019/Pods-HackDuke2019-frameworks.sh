9329b30e14c0630891598376ace1b5902f2940b6
codeplus <codeplus@CDSS-4494.wired.duke.local>
2019-11-03 13:40:07 -0500

    deleted pod

--- HackDuke2019/Pods/Target Support Files/Pods-HackDuke2019/Pods-HackDuke2019-frameworks.sh
+++ HackDuke2019/Pods/Target Support Files/Pods-HackDuke2019/Pods-HackDuke2019-frameworks.sh
@@ -163,7 +163,6 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ApiAI/ApiAI.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReverseExtension/ReverseExtension.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Toolbar/Toolbar.framework"
@@ -171,7 +170,6 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ApiAI/ApiAI.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReverseExtension/ReverseExtension.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Toolbar/Toolbar.framework"
